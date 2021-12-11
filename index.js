4ff6648f43984984675c6c3ed8521ac409b5add9
Ata Kuyumcu <atakuyumcu@fastmail.com>
2021-12-11 10:09:37 +0300

    implemented CLI functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { BitmexClient } from 'ccxws';
-import { BinarySearchTree } from '@datastructures-js/binary-search-tree';
-import chalk from 'chalk';
+import { BitmexClient } from "ccxws";
+import { BinarySearchTree } from "@datastructures-js/binary-search-tree";
+import chalk from "chalk";
 
 function diffColor(change, negate = false) {
   if (change < 0) {
@@ -25,8 +25,8 @@ class OrderBook {
     this.firstBid = { price: 0, size: 0 };
     this.difference = 0;
 
-    this.bitmex.on('l2snapshot', (snapshot) => this.handleL2Snapshot(snapshot));
-    this.bitmex.on('l2update', (update) => this.handleL2Update(update));
+    this.bitmex.on("l2snapshot", (snapshot) => this.handleL2Snapshot(snapshot));
+    this.bitmex.on("l2update", (update) => this.handleL2Update(update));
     this.bitmex.subscribeLevel2Updates(market);
   }
 
@@ -36,19 +36,19 @@ class OrderBook {
     const newDifference = parseFloat(newFirstAsk.price) - parseFloat(newFirstBid.price);
 
     if (newFirstAsk !== this.firstAsk || newFirstBid !== this.firstBid) {
-      process.stdout.write('First Ask: ');
-      process.stdout.write(diffColor(newFirstAsk.price - this.firstAsk.price, true)(`${this.market.quote} ${parseFloat(newFirstAsk.price).toFixed(2)}`));
-      process.stdout.write(' ');
+      process.stdout.write("First Ask: ");
+      process.stdout.write(diffColor(newFirstAsk.price - this.firstAsk.price, true)(`${this.market.quote} ${parseFloat(newFirstAsk.price)}`));
+      process.stdout.write(" ");
       process.stdout.write(diffColor(newFirstAsk.size - this.firstAsk.size)(Math.trunc(newFirstAsk.size)));
-      process.stdout.write('\t');
-      process.stdout.write('First Bid: ');
-      process.stdout.write(diffColor(newFirstBid.price - this.firstBid.price)(`${this.market.quote} ${parseFloat(newFirstBid.price).toFixed(2)}`));
-      process.stdout.write(' ');
+      process.stdout.write(" ");
+      process.stdout.write("First Bid: ");
+      process.stdout.write(diffColor(newFirstBid.price - this.firstBid.price)(`${this.market.quote} ${parseFloat(newFirstBid.price)}`));
+      process.stdout.write(" ");
       process.stdout.write(diffColor(newFirstBid.size - this.firstBid.size)(Math.trunc(newFirstBid.size)));
-      process.stdout.write('\t');
-      process.stdout.write('Difference: ');
-      process.stdout.write(diffColor(newDifference - this.difference)(`${this.market.quote} ${newDifference.toFixed(2)}`));
-      process.stdout.write('\n');
+      process.stdout.write(" ");
+      process.stdout.write("Difference: ");
+      process.stdout.write(diffColor(newDifference - this.difference)(`${this.market.quote} ${newDifference.toFixed(4)}`));
+      process.stdout.write("\n");
 
       this.firstAsk = newFirstAsk;
       this.firstBid = newFirstBid;
@@ -57,7 +57,7 @@ class OrderBook {
   }
 
   handleL2Snapshot(snapshot) {
-    for (const orderKey of ['asks', 'bids']) {
+    for (const orderKey of ["asks", "bids"]) {
       this[orderKey].clear();
 
       for (const order of snapshot[orderKey]) {
@@ -76,14 +76,14 @@ class OrderBook {
   }
 
   handleL2Update(update) {
-    for (const orderKey of ['asks', 'bids']) {
+    for (const orderKey of ["asks", "bids"]) {
       for (const order of update[orderKey]) {
         switch (order.meta.type) {
-          case 'insert':
-          case 'update':
+          case "insert":
+          case "update":
             this[orderKey].insert(order.meta.id, order);
             break;
-          case 'delete':
+          case "delete":
             this[orderKey].remove(order.meta.id);
             break;
           default:
@@ -96,5 +96,17 @@ class OrderBook {
   }
 }
 
+let [base, quote] = process.argv.slice(2);
+
+if (base === undefined) {
+  base = "XBT";
+  console.warn(`Base not specified, defaulting to ${base}`);
+}
+
+if (quote === undefined) {
+  quote = "USD";
+  console.warn(`Quote not specified, defaulting to ${quote}`);
+}
+
 /* eslint-disable no-unused-vars */
-const orderBook = new OrderBook({ id: 'XBTUSD', base: 'XBT', quote: 'USD' });
+const orderBook = new OrderBook({ id: `${base}${quote}`, base, quote });
